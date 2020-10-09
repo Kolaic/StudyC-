36632bfad938d2a2626f8d7fc3e9436a5130d4c8
kolai <janfun@foxmail.com>
2020-10-09 19:58:51 +0800

    init teleGrIm porject

---
+++ TeleGrIm/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Panuon.UI.Silver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeleGrIm
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : WindowX
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void WindowX_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
